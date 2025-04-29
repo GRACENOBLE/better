adf552469cfb94e636ac4ed9d999a45c146bd583
GRACENOBLE <gracenoble72@gmail.com>
2025-04-29 17:48:08 +0300

    feat(layout): integrate Header component into RootLayout

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,35 @@
 import Image from "next/image";
+import Container from "../common/container";
+import { Button, buttonVariants } from "../ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
   return (
-    <header>
-        <Image src={"/images/smile.jpg"} alt={""} width={500} height={500} className=""/>
+    <header className="border-b py-4">
+      <Container>
+        <div className="flex justify-between">
+          <Image
+            src={"/images/smile.jpg"}
+            alt={""}
+            width={500}
+            height={500}
+            className="h-12 object-contain"
+          />
+          <nav></nav>
+          <div>
+            <Link
+              href={"/sign-in"}
+              className={cn(
+                buttonVariants({ variant: "default", size: "lg" }),
+                "rounded-full"
+              )}
+            >
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
