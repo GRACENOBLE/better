157dcd03e51c58e06e3e4ac2788c5eb50ca0efa7
GRACENOBLE <gracenoble72@gmail.com>
2025-04-30 11:22:17 +0300

    feat: Add HeroSection component and enhance Header with responsive menu
    - Introduced HeroSection component for the main page layout.
    - Updated Header component to include a responsive navigation menu with scroll effects.
    - Implemented animated group and text effects for improved UI interactions.
    - Added InfiniteSlider and ProgressiveBlur components for dynamic logo display.
    - Enhanced overall styling and structure for better user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import HeroSection from "@/components/hero-section";
+
 const page = () => {
   return <div className="min-h-screen grid place-items-center">
-    
+    <HeroSection/>
   </div>;
 };
 
