c0893f210f29de8f0bebf29d8e26b24fb10267e5
GRACENOBLE <gracenoble72@gmail.com>
2025-05-31 11:44:43 +0300

    feat(hero): update HeroSection with new text content and button styles

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Mail, SendHorizonal } from "lucide-react";
+import { CornerRightUp, SendHorizonal } from "lucide-react";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { TextEffect } from "@/components/ui/text-effect";
 import { AnimatedGroup } from "@/components/ui/animated-group";
 import { LogoCloud } from "@/components/logo-cloud";
-import Image from "next/image";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
 import CustomButton from "./CustomButton";
 
 const transitionVariants = {
@@ -33,8 +31,8 @@ export default function HeroSection() {
   return (
     <>
       <section className="bg-accent">
-        <div className="relative mx-auto max-w-6xl px-6 pt-40 pb-32 2xl:pb-48 2xl:pt-48 ">
-          <div className="relative z-10 mx-auto max-w-4xl text-center">
+        <div className="mx-auto max-w-6xl px-6 pt-40 pb-32 2xl:pb-48 2xl:pt-48 ">
+          <div className=" mx-auto max-w-4xl text-center">
             <TextEffect
               preset="fade-in-blur"
               speedSegment={0.3}
@@ -59,9 +57,9 @@ export default function HeroSection() {
               as="p"
               className="mx-auto mt-12 max-w-2xl text-pretty text-lg"
             >
-              Embark on a journey of self discovery, create your own path, take
-              one step at a time and get one step closer each day to a better
-              you.
+              Increase your chances of
+              "Getting it done!". Plan with Better AI and create a custom roadmap with clearly
+              defined timelines and deliverables.
             </TextEffect>
 
             <AnimatedGroup
@@ -83,7 +81,7 @@ export default function HeroSection() {
               </Link>
 
               <form action="" className="mx-auto max-w-sm">
-                <div className="bg-background has-[input:focus]:ring-muted relative grid grid-cols-[1fr_auto] items-center rounded-md border pr-2 shadow shadow-zinc-950/5 has-[input:focus]:ring-2">
+                <div className="bg-background has-[input:focus]:ring-muted relative grid grid-cols-[1fr_auto] items-center rounded-full border pr-2 shadow shadow-zinc-950/5 has-[input:focus]:ring-2">
                   {/* <Mail className="pointer-events-none absolute inset-y-0 left-4 my-auto size-4" /> */}
 
                   <input
@@ -93,19 +91,18 @@ export default function HeroSection() {
                   />
 
                   <div className="md:pr-1.5 lg:pr-0">
-                    <Button
+                    <button
                       aria-label="submit"
-                      size="sm"
-                      className="rounded-sm"
+                      className="rounded-full bg-black text-white p-2"
                     >
                       <span className="hidden md:block">
-                        Start a conversation
+                        <CornerRightUp size={20} />
                       </span>
                       <SendHorizonal
                         className="relative mx-auto size-5 md:hidden"
                         strokeWidth={2}
                       />
-                    </Button>
+                    </button>
                   </div>
                 </div>
               </form>
