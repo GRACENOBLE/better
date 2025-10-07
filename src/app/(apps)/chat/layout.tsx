d2f9f4a73eb0b898f18da1ac180a191e844a3b72
GRACENOBLE <gracenoble72@gmail.com>
2025-10-07 20:52:19 +0300

    fix(auth): improve session handling and redirect to sign-in if not authenticated

--- src/app/(apps)/chat/layout.tsx
+++ src/app/(apps)/chat/layout.tsx
@@ -3,11 +3,15 @@ import { AISidebar } from "@/components/ai/ai-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "@/lib/auth/auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Layout = async({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth.api.getSession({
-      headers: await headers(),
-    });
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
   return (
     <SidebarProvider defaultOpen={false}>
       <AISidebar variant="inset" user={session?.user} />
